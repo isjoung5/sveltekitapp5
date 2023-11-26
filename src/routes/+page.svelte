03244756dedfc2e75cda7c30cedd05640f1d925c
JIS <isjoung5@gmail.com>
2023-11-26 22:10:46 +0900

    20231126-2210 변경 확인

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit Git 20231126-2150</h1>
+<h1>Welcome to SvelteKit Git 20231126-2209</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
